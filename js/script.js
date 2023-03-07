f18d76c377d2902ca4088973892e38149603a3c1
Maria Hola <maaria.hola@gmail.com>
2023-03-07 11:13:09 +0100

    swapping script.js to better version

--- js/script.js
+++ js/script.js
@@ -1,28 +1,30 @@
 console.log("Allohaaaaaa! Dziękuję za sprawdzenie mojego wesołego kantorka:)");
 
-let emptyElement = document.querySelector(".js-empty");
 let formElement = document.querySelector(".js-form");
-
-emptyElement.addEventListener("input", () => {
-    console.log(`Aktualna wartość do przewalutowania: ${emptyElement.value}`);
-});
-
-formElement.addEventListener("reset", () => {
-    console.log("Fromularz został zresetowany.");
-});
-
-let formElement = document.querySelector(".js-form");
-let emptyElement = document.querySelector(".js-empty");
+let plnElement = document.querySelector(".js-pln");
+let resultElement = document.querySelector(".js-result");
 let currencyElement = document.querySelector(".js-currency");
-let exchangeElement = document.querySelector(".js-exchange");
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-    let empty = emptyElement.value;
+let gramZlota = 258;
+let bitcoin = 98783.13;
+let akcjiOrlen = 64.74;
+
+formElement.addEventListener("input", () => {
+    let plnAmount = +plnElement.value;
     let currency = currencyElement.value;
-    
-    let exchange = empty / currency;
-    exchangeElement.innerText = exchange.toFixed(2);
+
+    switch (currency) {
+        case "gramZlota":
+            result = plnAmount / gramZlota;
+            break;
+        case "bitcoin":
+            result = plnAmount / bitcoin;
+            break;
+        case "akcjiOrlen":
+            result = plnAmount / akcjiOrlen;
+    }
+    if (result <= 0) {resultElement.innerText = "N/A"}
+    else { resultElement.innerText =`${plnAmount.toFixed(2)} PLN = ${result.toFixed(2)} ${currency}`};
 });
 
 
