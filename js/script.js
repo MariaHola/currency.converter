f2960d384a0bbdae5386732381b800b343500485
Maria Hola <maaria.hola@gmail.com>
2023-03-13 16:44:30 +0100

    JS added

--- js/script.js
+++ js/script.js
@@ -1,32 +1,50 @@
-console.log("Allohaaaaaa! Dziękuję za sprawdzenie mojego wesołego kantorka:)");
+{
 
-let formElement = document.querySelector(".js-form");
-let plnElement = document.querySelector(".js-pln");
-let resultElement = document.querySelector(".js-result");
-let currencyElement = document.querySelector(".js-currency");
+    const calculateResult = (plnAmount, currency) => {
+        const gramZlota = 258;
+        const bitcoin = 98437.77;
+        const akcjiOrlen = 65.64;
 
-let gramZlota = 258;
-let bitcoin = 98437.77; 
-let akcjiOrlen = 65.64;
+        switch (currency) {
+            case "gramZlota":
+                return plnAmount / gramZlota;
 
-formElement.addEventListener("input", () => {
-    let plnAmount = +plnElement.value;
-    let currency = currencyElement.value;
+            case "bitcoin":
+                return plnAmount / bitcoin;
 
-    switch (currency) {
-        case "gramZlota":
-            result = plnAmount / gramZlota;
-            break;
-        case "bitcoin":
-            result = plnAmount / bitcoin;
-            break;
-        case "akcjiOrlen":
-            result = plnAmount / akcjiOrlen;
+            case "akcjiOrlen":
+                return plnAmount / akcjiOrlen;
+        }
+    };
+
+    const updateResultText = (plnAmount, result, currency) => {
+        const resultElement = document.querySelector(".js-result");
+
+        if (result <= 0) { resultElement.innerText = "N/A" }
+        else { resultElement.innerText = `${plnAmount.toFixed(2)} PLN = ${result.toFixed(2)} ${currency}` };
     }
-    if (result <= 0) {resultElement.innerText = "N/A"}
-    else { resultElement.innerText =`${plnAmount.toFixed(2)} PLN = ${result.toFixed(2)} ${currency}`};
-});
 
+    const onFormInput = (event) => {
+        event.preventDefault();
 
+        const plnElement = document.querySelector(".js-pln");
 
+        const currencyElement = document.querySelector(".js-currency");
 
+        const plnAmount = +plnElement.value;
+        const currency = currencyElement.value;
+
+        const result = calculateResult(plnAmount, currency);
+
+        updateResultText(plnAmount, result, currency);
+    };
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+
+        formElement.addEventListener("input", onFormInput);
+    };
+
+    init();
+
+}
